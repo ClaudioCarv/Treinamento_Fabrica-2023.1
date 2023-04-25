0d050fe1005a283c20a667984690e69444910877
admin <admin>
2023-04-25 19:26:51 -0300

    atividade 2

---
+++ atividade_2.py
@@ -0,0 +1,10 @@
+import random
+
+# lista de alunos#
+alunos = ['Ana', 'Bruno', 'Carlos', 'Daniel', 'Elisa', 'Fernanda', 'Gustavo', 'Helena', 'Isabela', 'João']
+
+# sorteia um aluno aleatoriamente#
+aluno_sorteado = random.choice(alunos)
+
+# imprime o aluno sorteado#
+print("O aluno sorteado é:", aluno_sorteado)
