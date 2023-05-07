584eb107f884f5500a64975a1a83efc024e21184
admin <admin>
2023-05-07 20:56:16 -0300

    Exercicios_Demanda

--- Exercicio1_Demanda.py
+++ Exercicio1_Demanda.py
@@ -5,8 +5,8 @@ ano_atual = date.today().year
 idade = ano_atual - ano_nascimento
 
 if idade < 18:
-    print(f"Você tem {idade} anos. Ainda faltam {18 - idade} anos para você se alistar.")
+    print(f"Você tem {idade} anos. Ainda faltam {18 - idade} anos para você se alistar")
 elif idade == 18:
     print(f"Você tem {idade} anos. Está na hora de se alistar!")
 else:
-    print(f"Você tem {idade} anos. Já passou o tempo de alistamento há {idade - 18} anos.")
+    print(f"Você tem {idade} anos. Já passou o tempo de alistamento há {idade - 18} anos")
